5d2370b97c1be02ed1d209cffac67e703a827336
Jon Arild Torresdal <jon@torresdal.net>
2014-08-19 12:03:51 +0200

    Fixed bug related to UserData and WinRM firewall rule

--- Bootstrap/IOfferAwsBootstrapUserDataOptions.cs
+++ Bootstrap/IOfferAwsBootstrapUserDataOptions.cs
@@ -12,22 +12,24 @@ namespace ConDep.Dsl.Operations.Application.Local.Bootstrap.Aws
     {
         private readonly IOfferAwsBootstrapOptions _options;
         private readonly RunInstancesRequest _values;
+        private const string WINRM_FIREWALL_RULE = @"netsh advfirewall firewall add rule name=""WinRM Public in"" protocol=TCP dir=in profile=any localport=5985 remoteip=any localip=any action=allow";
 
         public AwsBootstrapUserDataOptions(RunInstancesRequest values, IOfferAwsBootstrapOptions options)
         {
             _options = options;
             _values = values;
+            _values.UserData = string.Format(@"<powershell>{0}</powershell>", WINRM_FIREWALL_RULE);
         }
 
         public IOfferAwsBootstrapOptions PowerShell(string script)
         {
-            _values.UserData = @"<powershell>netsh advfirewall firewall add rule name=""WinRM Public in"" protocol=TCP dir=in profile=any localport=5985 remoteip=any localip=any action=allow\n" + script + "</powershell>";
+            _values.UserData = string.Format("<powershell>{0}\n{1}</powershell>", WINRM_FIREWALL_RULE, script);
             return _options;
         }
 
         public IOfferAwsBootstrapOptions RunCmd(string script)
         {
-            _values.UserData = @"<script>netsh advfirewall firewall add rule name=""WinRM Public in"" protocol=TCP dir=in profile=any localport=5985 remoteip=any localip=any action=allow\n" + script + "</script>";
+            _values.UserData = string.Format("<script>{0}\n{1}</script>", WINRM_FIREWALL_RULE, script);
             return _options;
         }
     }
